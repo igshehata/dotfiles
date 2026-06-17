4d1443b529b33b1fdd1f60c94724e482a1adf637
Islam Shehata <islam.shehata@vodafone.com>
2026-06-17 16:26:53 +0300

    feat: tree aliases collapse dependency dirs instead of hiding them
    Replace lt/llt aliases with fish functions that use eza --ignore-glob
    to suppress contents of dependency/build dirs (node_modules, .next,
    target, .venv, etc.) while still listing them as collapsed entries.
    Only ignores dirs that actually exist in the target directory.

---
+++ dot_config/fish/functions/llt.fish
@@ -0,0 +1,31 @@
+function llt --description 'Detailed tree view (level 2) with collapsed dependency dirs'
+    set -l dep_dirs node_modules .next dist target .venv __pycache__ vendor .gradle build Pods .dart_tool .turbo .cache .nuxt .output .svelte-kit
+
+    set -l dir .
+    set -l pass_args
+    for arg in $argv
+        if test -d "$arg"
+            set dir $arg
+        end
+        set -a pass_args $arg
+    end
+
+    # Find which dep dirs actually exist
+    set -l found
+    for d in $dep_dirs
+        if test -d "$dir/$d"
+            set -a found $d
+        end
+    end
+
+    if test (count $found) -gt 0
+        set -l ignore_glob (string join '|' $found)
+        eza -lT --no-permissions --icons --level=2 --git -I "$ignore_glob" $pass_args
+        # Print collapsed dirs as a flat list at tree root level
+        for d in $found
+            echo "├── $d/"(set_color brblack)" [collapsed]"(set_color normal)
+        end
+    else
+        eza -lT --no-permissions --icons --level=2 --git $pass_args
+    end
+end
